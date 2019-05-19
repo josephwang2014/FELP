ee52b77b2b55cf3dd0d79a8709ab2fe3abddb4be
Zhiyi.Wang <zhiyi.wang@eosi.com.cn>
2019-05-20 00:40:09 +0800

    第五部分对象学习

---
+++ Head First JavaScript/Objects/Car.js
@@ -0,0 +1,20 @@
+const chevy = {
+    make: "Chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "red",
+    passengers: 2,
+    convertible: false,
+    mileage: 1021
+};
+const fiat = {
+    make: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "Medium Blue",
+    passengers: 2,
+    convertible: false,
+    mileage: 88000
+};
+fiat.mileage = 10000;
+fiat.needsWashing = true;
